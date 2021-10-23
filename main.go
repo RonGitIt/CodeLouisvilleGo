e1459b7cdb1c37159f40da9a32a0038b4c8b3c28
Jon Powers <Jon.K.Powers+git@gmail.com>
2021-10-22 22:39:15 -0400

    Added upload route; successfully getting file info

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,8 +10,29 @@ func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte("Hello world"))
 	} )
+	http.HandleFunc("/upload", handleImageUpload)
 	err := http.ListenAndServe(":5050", nil)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func handleImageUpload(w http.ResponseWriter, r *http.Request){
+	switch r.Method {
+	case http.MethodPost:
+		w.Write([]byte("That was a post to handleImageUpload"))
+		r.ParseMultipartForm(32 << 20)
+		file, header, err := r.FormFile("file")
+		if err != nil {
+			log.Printf("Error getting file from request: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+		w.Write([]byte(fmt.Sprintf("File info: %v", header.Size)))
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
 	}
 }
